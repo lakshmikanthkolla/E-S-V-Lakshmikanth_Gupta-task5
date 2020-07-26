7d254b9d43603111acb3092b04ccca375c41c7a5
lakshmikanthkolla <60390609+lakshmikanthkolla@users.noreply.github.com>
2020-07-26 21:28:53 +0530

    Add files via upload

---
+++ HouseCost.java
@@ -0,0 +1,20 @@
+package task_cleancode.task_cleancode;
+public class HouseConstructionCost{
+	public int costEstimation(String material_Type,int area,boolean is_automated){
+		int cost_per_square_feet=-1;
+		if(is_automated==true && material_Type.equals("High")) {
+			cost_per_square_feet=2500;
+		}
+		else if(material_Type.equals("High")) {
+			cost_per_square_feet=1800;
+		}
+		else if(material_Type.equals("Above")){
+			cost_per_square_feet=1500;
+		}
+		else if(material_Type.equals("Standard")){
+			cost_per_square_feet=1200;
+		}
+		return(cost_per_square_feet*area);
+	}
+
+}
